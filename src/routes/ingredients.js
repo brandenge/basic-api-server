126c23b07896855675b02563b9c9257a33bddc0f
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-09-23 02:32:08 -0500

    added validator and bug fixes

--- src/routes/ingredients.js
+++ src/routes/ingredients.js
@@ -1,56 +1,64 @@
 'use strict';
 
 const express = require('express');
-
+const { ingredientsValidator } = require('../middleware/validator');
 const { IngredientsModel } = require('../models');
 
 const router = express.Router();
 
-router.post('/ingredients', async (req, res, next) => {
-  console.log('Request Body:', req.body);
-  const newIngredient = await IngredientsModel.create(req.body);
-  res.status(200).send(newIngredient);
+router.post('/ingredients', ingredientsValidator, async (req, res, next) => {
+  try {
+    const newIngredient = await IngredientsModel.create(req.body);
+    res.status(200).send(newIngredient);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/ingredients', async (req, res, next) => {
-  console.log('Request Body:', req.body);
-  const allIngredients = await IngredientsModel.findAll();
-  res.status(200).send(allIngredients);
+  try {
+    const allIngredients = await IngredientsModel.findAll();
+    res.status(200).send(allIngredients);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/ingredients/:id', async (req, res, next) => {
-  console.log('Request Body:', req.body);
-  const oneIngredient = await IngredientsModel.findAll({
-    where: {
-      id: req.params.id,
-    },
-  });
-  res.status(200).send(oneIngredient);
+  try {
+    const oneIngredient = await IngredientsModel.findAll({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).send(oneIngredient);
+  } catch(error) {
+    next(error);
+  }
 });
 
 router.put('/ingredients/:id', async (req, res, next) => {
-  console.log('Request Body:', req.body);
-  await IngredientsModel.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  });
-  const updatedIngredients = await IngredientsModel.findAll({
-    where: {
-      id: req.params.id,
-    },
-  });
-  res.status(201).send(updatedIngredients);
+  try {
+    const { id } = req.params;
+    await IngredientsModel.update(req.body, { where: { id } });
+    const updatedIngredient = await IngredientsModel.findOne({ where: { id } });
+    res.status(201).send(updatedIngredient);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete('/ingredients/:id', async (req, res, next) => {
-  console.log('Request Body:', req.body);
-  await IngredientsModel.destroy({
-    where: {
-      id: req.params.id,
-    },
-  });
-  res.status(204).send('Success!');
+  try {
+    await IngredientsModel.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(204).send('Success! Ingredient deleted.');
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
